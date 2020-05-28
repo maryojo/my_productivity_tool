a246d6f3b979a2d68f6a0f0f1c314e433e9a14f5
Mary Ojo <38431519+oebunoluwa@users.noreply.github.com>
2020-05-28 19:53:30 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+var myusername = "new-user";
+var password = "password";
+
+var usernameEntry = document.getElementById("login-username");
+var passwordEntry = document.getElementById("password");
+var loginButton = document.getElementById("submit");
+
+
+
+
+function checkUser(){
+if (usernameEntry.value === myusername && passwordEntry.value === password){
+    window.location = "home_index.html";
+} else{
+    //onclick of submit
+    alert("Sorry, wrong username or password");
+}
+}
+loginButton.addEventListener("click", checkUser);
